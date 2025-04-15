6779545e0ad88c28bf3608a6e188a415017b7117
sydammani2003 <sydammani2003@gmail.com>
2025-04-15 12:18:43 +0530

    Implmented Bloc for counter app successfully

--- bloc_learning/lib/main.dart
+++ bloc_learning/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Counter App Implemented using Bloc'),
     );
   }
 }
