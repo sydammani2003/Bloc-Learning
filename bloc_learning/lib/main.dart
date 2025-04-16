4a782a8c9033a4ca31f0fd61d76b0f84a8d2ef92
sydammani2003 <sydammani2003@gmail.com>
2025-04-16 18:24:45 +0530

    Implmented Bloc for counter app successfully

--- bloc_learning/lib/main.dart
+++ bloc_learning/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Counter App Implemented using Bloc'),
+      home: const MyHomePage(title: 'Counter App'),
     );
   }
 }
@@ -33,11 +33,11 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  final String title; 
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-  }
+}
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
